c53e10b69975bbf103e5685ff1aa93ac4daedbe9
Chen_C <hichen3dem@gmail.com>
2016-06-15 17:01:18 -0500

    Update and rename printctf.sh to printctf2file.sh

--- printctf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in *.txt
-do
-a=$(awk 'NR==6{ print $7 }' $i)
-echo "$i" "$a"
-done
---
+++ printctf2file.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+touch ctfprint                                        # create a new file 
+for i in *.txt
+do
+a=$(awk 'NR==6{ print $7 }' $i)
+echo "$i $a" >> ctfprint                              # add output to the end of ctfprint
+done
