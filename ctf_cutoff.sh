e4e56d015fd9175945e692b9fe4a1aad4ae64a4b
Chen_C <hichen3dem@gmail.com>
2016-06-15 16:05:05 -0500

    Update ctf_cutoff.sh

--- ctf_cutoff.sh
+++ ctf_cutoff.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+touch cutoff8
 for i in *.txt
 do
 a=$(awk 'NR==6{ print $7 }' $i)                      # output value in line6 (NR==6) and column7 ($7) to a variable a
 if (( $(bc <<< "$a <= 8") ))                         # floating points comparison (8 is the cutoff ctf value) 
 then
-  mv $i selebetterctf8                                  # should make selebetterctf8 directory first 
+  echo "$i" >> cutoff8                                  # should make cutoff8 file first 
 fi
 done
