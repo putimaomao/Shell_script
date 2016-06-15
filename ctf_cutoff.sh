e530da3196c40505c873895b54b67cea12578cdb
Chen_C <hichen3dem@gmail.com>
2016-06-15 16:56:01 -0500

    Update ctf_cutoff.sh

--- ctf_cutoff.sh
+++ ctf_cutoff.sh
@@ -6,6 +6,6 @@ do
 a=$(awk 'NR==6{ print $7 }' $i)                      # output value in line6 (NR==6) and column7 ($7) to the variable a
 if (( $(bc <<< "$a <= 8") ))                         # floating points comparison (8 is the cutoff ctf value) 
 then
-  echo "$i" >> cutoff8                               # should make cutoff8 file first 
+  echo "$i" >> cutoff8                               # add $i to the end of the file cutoff8
 fi
 done
