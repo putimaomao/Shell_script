096014fb42095129ee43112b48dd8e922c0790a3
Chen_Cui <hichen3dem@gmail.com>
2019-10-15 15:25:20 -0500

    Add files via upload

--- star_for_extraction.pl
+++ star_for_extraction.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-# perl sccript star outfolder box_size
+# perl sccript star outfolder
 use strict;
 use File::Basename;
 
@@ -15,8 +15,6 @@ my $star_in = $ARGV[0];
 my $box_out = $ARGV[1];
 mkdir($box_out);
 
-my $box_size = $ARGV[2];
-
 my %record;
 
 open(IN, "$star_in") or die;
@@ -25,15 +23,11 @@ while (<IN>) {
         my @arr =split(/\s+/, $_);
         my $coorx = trim($arr[1]);
         my $coory = trim($arr[2]);
-        $coorx = $coorx - ($box_size/2);
-        $coory = $coory - ($box_size/2);
-        #$coorx = substr($coorx, 0, length($coorx)-7) - ($box_size/2);
-        #$coory = substr($coory, 0, length($coory)-7) - ($box_size/2);
         my $path = trim($arr[0]);
-        print $path."\n";
+        #print $path."\n";
         my $file = basename($path);
         print $file."\n";
-        push (@{$record{$file}}, "$coorx\t$coory\t$box_size\t$box_size\n");        
+        push (@{$record{$file}}, "$coorx\t$coory\n");        
     }
 }
 close IN;
@@ -42,9 +36,16 @@ chdir($box_out);
 my @key = keys(%record);
 foreach my $name (@key){
     my $box_name = substr ($name ,0, length($name)-4);
-    open(OUT, ">$box_name.box") or die;
+    open(OUT, ">$box_name.star") or die;
+    print OUT "# RELION; version 3.0-beta-2
+
+data_
+
+loop_ 
+_rlnCoordinateX #1 
+_rlnCoordinateY #2 \n";
     foreach my $i (@{$record{$name}}){
-        print OUT $i;
+        print OUT " $i";
     }
     close OUT;
 }   
